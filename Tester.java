dc01df08478862340c64aab50d3667812b94b080
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-10 20:37:31 +0000

    Add 3 arithmetic problems

--- Tester.java
+++ Tester.java
@@ -4,4 +4,8 @@ public class Tester
  {
   public static void main(String args[])
    {
-    
+    System.out.println(79 + 3 * (4 + 82 - 68) - 7 + 19) //145
+    System.out.println((179 + 21 + 10) / 7 + 181)//211
+    System.out.println(10389 * 56 * 11 + 2246)//6401870
+   }
+ }
